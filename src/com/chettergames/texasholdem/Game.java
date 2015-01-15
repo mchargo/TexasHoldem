481178afdcd7475e6408c6d6050403839c647e93
John Detter <john@detter.com>
2015-01-15 16:06:43 -0600

    Added lots of stuff for mike to do

--- src/com/chettergames/texasholdem/Game.java
+++ src/com/chettergames/texasholdem/Game.java
@@ -1,61 +1,249 @@
 package com.chettergames.texasholdem;
 
+import java.io.IOException;
 import java.util.Arrays;
+
+import com.chettergames.net.Output;
 
 public class Game 
 {
-	public Game(int playerCount)
+	public Game(int playerCount, int startingChips)
 	{
 		players = new Player[playerCount];
-		Arrays.fill(null, players);
+		Arrays.fill(players, null);
+		this.startChips = startingChips;
 	}
-	
-	public synchronized void addPlayer(Player p)
-	{
-		
-	}
-	
-	public synchronized void removePlayer(Player p)
-	{
-		
-	}
-	
+
 	public int playerCount()
 	{
+		/**
+		 * Implement this first.
+		 * 
+		 * Count the players in the 'players' array.
+		 * 
+		 * How do I do this?
+		 * 
+		 * For each element in the players array,
+		 * check if it is equal to null. If it is
+		 * not equal to null, there is a player
+		 * in that position. If the element is
+		 * equal to null, that position is open
+		 * for another player to join. The length
+		 * of the players array can be found using:
+		 * 
+		 * int playersLength = players.length;
+		 * 
+		 * NOTE: only count a player if they are ready
+		 * to play.
+		 */
+
 		return 0;
 	}
-	
-	public void dealStart()
+
+	public synchronized boolean addPlayer(Player p)
 	{
-		//deals starting hand to each player
+		/**
+		 * Add a player to the game.
+		 * 
+		 * How do I do this?
+		 *
+		 * Find a position in the array where
+		 * the element is equal to null. If
+		 * there are no such positions, then
+		 * the game is full and we should
+		 * return false. If we are able to
+		 * add a player to the array, return
+		 * true.
+		 * 
+		 * make sure you call promptForName()
+		 * on each player when they are added.
+		 */
+
+		return false;
 	}
-	public void dealFlop()
+
+	public synchronized void removePlayer(Player p)
 	{
-		
+		/**
+		 * Remove the given player from the
+		 * array. In order to remove a player
+		 * from the array, search for where
+		 * the player is in the array, then
+		 * set that position to null.
+		 * 
+		 * For example, to test position 3:
+		 * 
+		 * boolean equal = players[3] == p;
+		 * 
+		 * if(equal)
+		 * {
+		 *     ... 
+		 * }
+		 */
+
+
 	}
-	
-	public void dealTurn()
+
+	public void prepareForNewRound()
 	{
-		//deals the Turn after a round of betting
+		/**
+		 * Tell each player that a new round
+		 * of poker is about to start.
+		 * 
+		 * How do I do this?
+		 * 
+		 * For each element in players,
+		 * if the element is not equal to null
+		 * we know there is a valid player
+		 * there. Tell that player to prepare
+		 * for a new round by calling:
+		 * 
+		 * player.newRound();
+		 * 
+		 * Also you should clear all of the
+		 * cards on the table and reset the pot.
+		 */
+
+
 	}
-	public void dealRiver()
+
+	public void getAntes()
 	{
-		//deals the River after a round of betting
+		/**
+		 * Get the antes from all of the players
+		 * and add the result to the pot.
+		 * 
+		 * How do I do this?
+		 * 
+		 * Ask each player for their ante using:
+		 * 
+		 * int bet = player.getAnte();
+		 * 
+		 * If the player is folded, do not ask
+		 * them for their ante.
+		 */
 	}
-	
+
+	public void dealStart(Deck deck)
+	{
+		/**
+		 * Deal 2 cards to each player like
+		 * you would in poker.
+		 * 
+		 * How do I do this?
+		 * 
+		 * In poker, you deal one card to each
+		 * player at a time in a circle, so
+		 * for each player that is playing
+		 * we should deal one card to them.
+		 * Once we are done with that, we should
+		 * deal them all another card.
+		 * 
+		 * 
+		 */
+	}
+
 	public void playRound()
 	{
-		//each player completes a turn, either checking, betting, raising, or folding
+		/**
+		 * Do a round of betting in poker.
+		 */
 	}
+
+
+
+	public void dealFlop(Deck deck)
+	{
+		/**
+		 * Put the flop onto the table
+		 */
+	}
+
+	public void dealTurn(Deck deck)
+	{
+
+	}
+
+	public void dealRiver(Deck deck)
+	{
+
+	}
+
+
 	public void checkForHighestHand()
 	{
-		//after the final playRound occurs, method checks for winner
 	}
-	
+
+	/**
+	 * Play the game on a new Thread.
+	 */
+	public void postPlayGame()
+	{
+		new Thread(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				playGame();
+			}
+		}).start();
+	}
+
+	/**
+	 * Game play loop.
+	 */
+	public void playGame()
+	{	
+		while(true)
+		{
+			if(playerCount() < 2)
+			{
+				try
+				{
+					Output.gameln("We must wait for more players to be ready...");
+					Thread.sleep(5000);
+					continue;
+				}catch(Exception e){e.printStackTrace();}
+			}
+			
+			// check for people who need to join the game.
+			for(Player p : players)
+				if(p.isReady() && !p.isPlaying())
+					p.joinGame(startChips);
+
+
+			Deck deck = new Deck();
+
+			prepareForNewRound(); // prepare for new round
+			getAntes(); // get ante from players
+			dealStart(deck); // deal cards to players
+			playRound(); // play a round of betting
+			dealFlop(deck); // deal the flop
+			playRound(); // play a round of betting
+			dealTurn(deck); // deal the turn
+			playRound(); // play a round of betting
+			dealRiver(deck); // deal the river
+			playRound(); // play the last round of betting
+
+			checkForHighestHand(); // check for winner
+		}
+	}
+
+	public void newTestGame()
+	{
+		for(int x = 0;x < players.length;x++)
+			addPlayer(new ComputerPlayer(x));
+	}
+
+	private Card[] tableCards;
 	private Player[] players;
-	
+	private int pot;
+	private int startChips;
+
 	public static void main(String args[])
 	{
-		Game game = new Game(6);
+		Game game = new Game(6, 1000);
+		game.newTestGame();
+		game.postPlayGame();
 	}
 }
--- src/com/chettergames/texasholdem/Player.java
+++ src/com/chettergames/texasholdem/Player.java
@@ -1,13 +1,40 @@
 package com.chettergames.texasholdem;
 
+import com.chettergames.net.Output;
+
 public abstract class Player 
 {
 	public Player(int number)
 	{
 		this.number = number;
 		folded = true;
+		playing = false;
 	}
 
+	/**
+	 * newRound should be called at the
+	 * begging of each new round of
+	 * poker. This should be called
+	 * before getAnte().
+	 */
+	public void newRound()
+	{
+		card1 = null;
+		card2 = null;
+		myRoundBet = 0;
+		folded = false;
+
+		if(chips == 0 && !isReady()) 
+			folded = true;
+	}
+
+	/**
+	 * This is called to get the ante
+	 * from the player.
+	 * 
+	 * @param ante The ante amount
+	 * @return How much the player can ante.
+	 */
 	public int ante(int ante)
 	{
 		if(chips == 0)
@@ -27,44 +54,94 @@ public abstract class Player
 		return ante;
 	}
 
+	/**
+	 * Call this when the player has won
+	 * the pot.
+	 * @param chipsWon
+	 */
 	public void wonPot(int chipsWon)
 	{
 		chips += chipsWon;
 	}
 
+	/**
+	 * Call this when a new round of
+	 * betting has started.
+	 */
 	public void newBettingRound()
 	{
 		myRoundBet = 0;
 	}
 
-	public void getCards(Card card1, Card card2)
+	/**
+	 * Call this when the player is supposed
+	 * to receive cards after the ante.
+	 * 
+	 * @param card1 The first card
+	 * @param card2 The second card.
+	 */
+	public void recieveCards(Card card1, Card card2)
 	{
-		this.card1 = card1;
-		this.card2 = card2;
+		recieveCard(card1);
+		recieveCard(card2);
 	}
 
+	/**
+	 * Get dealt one card.
+	 * @param card
+	 */
+	public void recieveCard(Card card)
+	{
+		if(card1 == null)
+			card1 = card;
+		else if(card2 == null)
+			card2 = card;
+	}
+
+	/**
+	 * Call this when the player has connected
+	 * and is ready to play the game.
+	 * 
+	 * @param startChips Chips to start with.
+	 */
 	public void joinGame(int startChips)
 	{
+		playing = true;
 		this.chips = startChips;
 	}
 
-	// -1 = fold
+	/**
+	 * Get the bet during a round of betting.
+	 * @param currentBet
+	 * @return The player's bet. -1 if the player folds.
+	 */
 	public abstract int getBet(int currentBet);
+
+	/**
+	 * Tell the player to give us their name.
+	 */
 	public abstract void promptForName();
+
+	/**
+	 * Is the player ready?
+	 * @return Whether or not the player is ready.
+	 */
 	public abstract boolean isReady();
-	
+
 	public Card getCard1(){return card1;}
 	public Card getCard2(){return card2;}
 	public int getRoundBet(){return myRoundBet;}
 	public boolean isFolded(){return folded;}
 	public String getName(){return name;}
 	public int getNumber(){return number;}
+	public boolean isPlaying(){return playing;}
 
-	private int chips;
-	private String name;
-	private int number;
-	private Card card1;
-	private Card card2;
-	private int myRoundBet;
-	private boolean folded;
+	protected int chips;
+	protected String name;
+	protected int number;
+	protected Card card1;
+	protected Card card2;
+	protected int myRoundBet;
+	protected boolean folded;
+	protected boolean playing;
 }
