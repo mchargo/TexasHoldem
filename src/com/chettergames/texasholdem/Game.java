885d4df7382126debe568775027781e69371077c
John Detter <john@detter.com>
2015-01-17 23:27:11 -0600

    Added Hand class

--- src/com/chettergames/texasholdem/Game.java
+++ src/com/chettergames/texasholdem/Game.java
@@ -176,8 +176,10 @@ public class Game
 	public Player findWinner()
 	{	
 		int hands[] = new int[players.length];
+		int value[] = new int[players.length];
 		Arrays.fill(hands, -1);
-		
+		Arrays.fill(value, -1);
+
 		for(int player = 0;player < players.length;player++)
 		{
 			Card hand[] = new Card[tableCards.length + 2];
@@ -185,218 +187,16 @@ public class Game
 				hand[x] = tableCards[x];
 			hand[tableCards.length] = players[player].getCard1();
 			hand[tableCards.length + 1] = players[player].getCard2();
-			
+
 			if(checkForRoyalFlush(hand))
 				hands[player] = ROYAL_FLUSH;
 			else if(checkForStraightFlush(hand) != -1)
 				hands[player] = STRAIGHT_FLUSH;
-			
+
 			// continue here
 		}
 
 		return players[0];
-	}
-	
-	public int checkForHighCard(Card cards[])
-	{
-		int vals[] = new int[Card.CARDS_IN_SUIT];
-		for(Card c : cards)
-			vals[c.getValue() - 2]++;
-		for(int x = vals.length -1; x >= 0; x--)
-			if(vals[x] > 0)
-				return (x+2);
-			
-		return -1;
-	}
-
-	public int checkForPair(Card cards[])
-	{
-		int vals[] = new int[Card.CARDS_IN_SUIT];
-		for(Card c : cards)
-			vals[c.getValue() - 2]++;
-
-		for(int x = vals.length - 1;x >= 0;x--)
-			if(vals[x] > 2)
-				return (x + 2);
-		return -1;
-	}
-
-	public int checkForTwoPair(Card cards[])
-	{
-		int pairs = 0;
-		int vals[] = new int[Card.CARDS_IN_SUIT];
-		for(Card c : cards)
-			vals[c.getValue() - 2]++;
-
-		for(int x = 0;x <= vals.length-1;x++)
-		{
-			if(vals[x] > 1)
-				pairs++;
-			if(pairs>=2)
-				return (x + 2);
-		}
-
-		return -1;
-	}
-
-	/**
-	 * Return the highest value 3 of a kind in your hand.
-	 * -1 for no three of a kind
-	 * 
-	 * @param cards
-	 * @return The value of the highest 3 of a kind.
-	 */
-	public int checkForThreeOfAKind(Card cards[])
-	{
-		int vals[] = new int[Card.CARDS_IN_SUIT];
-		for(Card c : cards)
-			vals[c.getValue() - 2]++;
-
-		for(int x = vals.length - 1;x >= 0;x--)
-			if(vals[x] > 2)
-				return (x + 2);
-
-		return -1;
-	}
-
-	/**
-	 * Check cards for a straight
-	 * @param cards The cards to check
-	 * @return The value of the highest card, -1 if no straight
-	 */
-	public int checkForStraight(Card cards[])
-	{
-		LinkedList<Integer> vals = new LinkedList<Integer>();
-		for(Card c : cards)
-			vals.add(c.getValue());
-		Collections.sort(vals);
-
-		// remove doubles
-		for(int x = 0;x < vals.size() - 1;x++)
-		{
-			if(vals.get(x) == vals.get(x + 1))
-			{
-				vals.remove(x);
-				x--;
-			}
-		}
-
-		if(vals.size() < 5) return -1;
-		int longestStreak = 0;
-		int highestCard = 0;
-
-		int currStreak = 0;
-		int lastVal = 0;
-
-		// ace can also be 1
-		if(vals.getLast() == Card.VAL_ACE)
-		{
-			lastVal = 1;
-			currStreak = 1;
-		}
-
-		Iterator<Integer> it = vals.iterator();
-		while(it.hasNext())
-		{
-			int val = it.next();
-			if(currStreak == 0)
-			{
-				currStreak++;
-				lastVal = val;
-			} else {
-				if(val == lastVal + 1)
-				{
-					currStreak++;
-					lastVal = val;
-
-					if(currStreak > 4)
-					{
-						highestCard = val;
-						longestStreak = currStreak;
-					}
-				} else {
-					currStreak = 1;
-					lastVal = val;
-				}
-			}
-		}
-
-		if(longestStreak > 4)
-			return highestCard;
-		return -1;
-	}
-
-	public int checkForFlush(Card cards[])
-	{
-		int suits[] = new int[4];
-		for(Card c : cards)
-			suits[Card.typeToVal(c.getType())]++;
-		for(int x = 0;x < suits.length;x++)
-			if(suits[x] > 4) return x; 
-		return -1;
-	}
-
-	public int checkForFullHouse(Card cards[])
-	{
-		int vals[] = new int[Card.CARDS_IN_SUIT];
-		for(Card c : cards)
-			vals[c.getValue() - 2]++;
-
-		boolean three = false;
-		boolean pair = false;
-		int val3 = 0;
-
-		for(int x = vals.length - 1;x >= 0;x--)
-			if(vals[x] > 2 && !three)
-			{
-				three = true;
-				val3 = x + 2;
-			} else {
-				if(vals[x] > 1)
-					pair = true;
-			}
-
-		if(three && pair)
-			return val3;
-
-		return -1;
-	}
-
-	public int checkForFourOfAKind(Card cards[])
-	{
-		int vals[] = new int[Card.CARDS_IN_SUIT];
-		for(Card c : cards)
-			vals[c.getValue() - 2]++;
-
-		for(int x = vals.length - 1;x >= 0;x--)
-			if(vals[x] > 3)
-				return (x + 2);
-		return -1;
-	}
-
-	public int checkForStraightFlush(Card cards[])
-	{
-		int suit = checkForFlush(cards);
-		if(suit == -1) return -1;
-		LinkedList<Card> remainingCards = new LinkedList<Card>();
-		for(Card c : cards)
-			if(Card.typeToVal(c.getType()) == suit)
-				remainingCards.add(c);
-		
-		cards = new Card[remainingCards.size()];
-		for(int x = 0;x < remainingCards.size();x++)
-			cards[x] = remainingCards.get(x);
-		
-		return checkForStraight(cards);
-	}
-
-	public boolean checkForRoyalFlush(Card cards[])
-	{
-		if(checkForStraightFlush(cards)==14)
-		{
-			return true;
-		}
-		return false;
 	}
 
 
@@ -493,22 +293,10 @@ public class Game
 	private int startChips;
 	private int startingAnte;
 
-	public static final int NO_HAND 		= -1;
-	public static final int HIGH_CARD 		= 0;
-	public static final int PAIR 			= 1;
-	public static final int TWO_PAIR		= 2;
-	public static final int THREE_OF_A_KIND = 3;
-	public static final int STRAIGHT		= 4;
-	public static final int FLUSH			= 5;
-	public static final int FULL_HOUSE		= 6;
-	public static final int FOUR_OF_A_KIND	= 7;
-	public static final int STRAIGHT_FLUSH	= 8;
-	public static final int ROYAL_FLUSH		= 9;
-
 	public static void main(String args[])
 	{
 		Game game = new Game(6, 1000, 10);
-		
+
 		Card card1 = new Card(Card.VAL_2, Card.Type.SPADES);
 		Card card2 = new Card(Card.VAL_JACK, Card.Type.HEARTS);
 		Card card3 = new Card(Card.VAL_QUEEN, Card.Type.HEARTS);
@@ -516,9 +304,9 @@ public class Game
 		Card card5 = new Card(Card.VAL_8, Card.Type.CLUBS);
 		Card card6 = new Card(Card.VAL_9, Card.Type.HEARTS);
 		Card card7 = new Card(Card.VAL_3, Card.Type.CLUBS);
-		
+
 		Card cards[] = new Card[]{card1, card2, card3, card4, card5, card6, card7};
-		int result = game.checkForHighCard(cards);
+		int result = 0;//game.checkForHighCard(cards);
 		if(result >= 0)
 			System.out.println("highest card: " + result);
 		else System.out.println("No hand.");
---
+++ src/com/chettergames/texasholdem/Hand.java
@@ -0,0 +1,246 @@
+package com.chettergames.texasholdem;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Hand 
+{
+	public Hand(Card card1, Card card2, Card tableCards[])
+	{
+		
+	}
+	
+	public void calculateHand()
+	{
+		
+	}
+	
+	public int getType()
+	{
+		return type;
+	}
+	
+	public int getValue()
+	{
+		return value;
+	}
+	
+	public int checkForHighCard(Card cards[])
+	{
+		int vals[] = new int[Card.CARDS_IN_SUIT];
+		for(Card c : cards)
+			vals[c.getValue() - 2]++;
+		for(int x = vals.length -1; x >= 0; x--)
+			if(vals[x] > 0)
+				return (x+2);
+
+		return -1;
+	}
+
+	public int checkForPair(Card cards[])
+	{
+		int vals[] = new int[Card.CARDS_IN_SUIT];
+		for(Card c : cards)
+			vals[c.getValue() - 2]++;
+
+		for(int x = vals.length - 1;x >= 0;x--)
+			if(vals[x] > 2)
+				return (x + 2);
+		return -1;
+	}
+
+	public int checkForTwoPair(Card cards[])
+	{
+		int pairs = 0;
+		int vals[] = new int[Card.CARDS_IN_SUIT];
+		for(Card c : cards)
+			vals[c.getValue() - 2]++;
+
+		for(int x = 0;x <= vals.length-1;x++)
+		{
+			if(vals[x] > 1)
+				pairs++;
+			if(pairs>=2)
+				return (x + 2);
+		}
+
+		return -1;
+	}
+
+	/**
+	 * Return the highest value 3 of a kind in your hand.
+	 * -1 for no three of a kind
+	 * 
+	 * @param cards
+	 * @return The value of the highest 3 of a kind.
+	 */
+	public int checkForThreeOfAKind(Card cards[])
+	{
+		int vals[] = new int[Card.CARDS_IN_SUIT];
+		for(Card c : cards)
+			vals[c.getValue() - 2]++;
+
+		for(int x = vals.length - 1;x >= 0;x--)
+			if(vals[x] > 2)
+				return (x + 2);
+
+		return -1;
+	}
+
+	/**
+	 * Check cards for a straight
+	 * @param cards The cards to check
+	 * @return The value of the highest card, -1 if no straight
+	 */
+	public int checkForStraight(Card cards[])
+	{
+		LinkedList<Integer> vals = new LinkedList<Integer>();
+		for(Card c : cards)
+			vals.add(c.getValue());
+		Collections.sort(vals);
+
+		// remove doubles
+		for(int x = 0;x < vals.size() - 1;x++)
+		{
+			if(vals.get(x) == vals.get(x + 1))
+			{
+				vals.remove(x);
+				x--;
+			}
+		}
+
+		if(vals.size() < 5) return -1;
+		int longestStreak = 0;
+		int highestCard = 0;
+
+		int currStreak = 0;
+		int lastVal = 0;
+
+		// ace can also be 1
+		if(vals.getLast() == Card.VAL_ACE)
+		{
+			lastVal = 1;
+			currStreak = 1;
+		}
+
+		Iterator<Integer> it = vals.iterator();
+		while(it.hasNext())
+		{
+			int val = it.next();
+			if(currStreak == 0)
+			{
+				currStreak++;
+				lastVal = val;
+			} else {
+				if(val == lastVal + 1)
+				{
+					currStreak++;
+					lastVal = val;
+
+					if(currStreak > 4)
+					{
+						highestCard = val;
+						longestStreak = currStreak;
+					}
+				} else {
+					currStreak = 1;
+					lastVal = val;
+				}
+			}
+		}
+
+		if(longestStreak > 4)
+			return highestCard;
+		return -1;
+	}
+
+	public int checkForFlush(Card cards[])
+	{
+		int suits[] = new int[4];
+		for(Card c : cards)
+			suits[Card.typeToVal(c.getType())]++;
+		for(int x = 0;x < suits.length;x++)
+			if(suits[x] > 4) return x; 
+		return -1;
+	}
+
+	public int checkForFullHouse(Card cards[])
+	{
+		int vals[] = new int[Card.CARDS_IN_SUIT];
+		for(Card c : cards)
+			vals[c.getValue() - 2]++;
+
+		boolean three = false;
+		boolean pair = false;
+		int val3 = 0;
+
+		for(int x = vals.length - 1;x >= 0;x--)
+			if(vals[x] > 2 && !three)
+			{
+				three = true;
+				val3 = x + 2;
+			} else {
+				if(vals[x] > 1)
+					pair = true;
+			}
+
+		if(three && pair)
+			return val3;
+
+		return -1;
+	}
+
+	public int checkForFourOfAKind(Card cards[])
+	{
+		int vals[] = new int[Card.CARDS_IN_SUIT];
+		for(Card c : cards)
+			vals[c.getValue() - 2]++;
+
+		for(int x = vals.length - 1;x >= 0;x--)
+			if(vals[x] > 3)
+				return (x + 2);
+		return -1;
+	}
+
+	public int checkForStraightFlush(Card cards[])
+	{
+		int suit = checkForFlush(cards);
+		if(suit == -1) return -1;
+		LinkedList<Card> remainingCards = new LinkedList<Card>();
+		for(Card c : cards)
+			if(Card.typeToVal(c.getType()) == suit)
+				remainingCards.add(c);
+
+		cards = new Card[remainingCards.size()];
+		for(int x = 0;x < remainingCards.size();x++)
+			cards[x] = remainingCards.get(x);
+
+		return checkForStraight(cards);
+	}
+
+	public boolean checkForRoyalFlush(Card cards[])
+	{
+		if(checkForStraightFlush(cards)==14)
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	private Card cards[];
+	private int value;
+	private int type;
+	
+	public static final int NO_HAND 		= -1;
+	public static final int HIGH_CARD 		= 0;
+	public static final int PAIR 			= 1;
+	public static final int TWO_PAIR		= 2;
+	public static final int THREE_OF_A_KIND = 3;
+	public static final int STRAIGHT		= 4;
+	public static final int FLUSH			= 5;
+	public static final int FULL_HOUSE		= 6;
+	public static final int FOUR_OF_A_KIND	= 7;
+	public static final int STRAIGHT_FLUSH	= 8;
+	public static final int ROYAL_FLUSH		= 9;
+}
